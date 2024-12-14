962ba061b485b81da4f4ba612700dc835abcf372
andres <andresdavid.ortega@gmail.com>
2024-12-14 16:28:36 -0500

    modificacion panel de verificacion de Email

--- src/components/login/EmailVerification.jsx
+++ src/components/login/EmailVerification.jsx
@@ -1,11 +1,13 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { authService } from '../../services/authService';
+import { useNavigate } from 'react-router-dom';
 
 
 export function EmailVerification() {
   const { token } = useParams();
   const [status, setStatus] = useState('Verificando...');
+  const navigate=useNavigate()
 
   useEffect(() => {
     const verifyToken = async () => {
@@ -20,6 +22,9 @@ export function EmailVerification() {
     verifyToken();
   }, [token]);
 
-  return <div className='text-black'><h1>{status}</h1></div>;
+  return <div className='text-black text-xl font-bold'>
+    <h1>{status}</h1>
+    <button  className='bg-[#0FA0CC] p-3 rounded-lg w-full hover:bg-[#0c88ad] hover:cursor-pointer transition duration-300' onClick={()=>navigate('/client')}>Volver al Comercio</button>
+    </div>;
 }
 
