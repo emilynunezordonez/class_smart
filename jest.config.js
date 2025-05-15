2cd609b46f1b8dca30835f98a74d83a337db35cb
Vovleras <victoria.volveras@correounivalle.edu.co>
2025-05-15 10:30:49 -0500

    Tests for components/login

--- jest.config.js
+++
@@ -1,4 +0,0 @@
-export const setupFilesAfterEnv = ["<rootDir>/jest.setup.js"];
-export const collectCoverage = true;
-export const collectCoverageFrom = ["src/**/*.{js,jsx}"];
-export const transform =   {"^.+\\.[t|j]sx?$": "babel-jest"};
